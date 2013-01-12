9c70bb8b9cb12ba2e45b369c9e4c44173950c58d
John Ferlito <johnf@inodes.org>
2013-01-12 13:08:10 +1100

    Add rails 3.2.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoR::Application.config.secret_token = '0faf4d0b5b779b52c29a05780a91069002ac1bc02e759ae24dfd33f0754035f7fcf5b2c01c92e83defb7af5516bc69c370bcff3aa095ac6518243a6604a72cd4'

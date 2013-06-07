3ccee46499b2fc23cd3e8621cd656c32b2cf8047
John Ferlito <johnf@inodes.org>
2013-06-07 17:12:14 +1000

    Rails 3.1.0

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RoR::Application.config.secret_token = '0faf4d0b5b779b52c29a05780a91069002ac1bc02e759ae24dfd33f0754035f7fcf5b2c01c92e83defb7af5516bc69c370bcff3aa095ac6518243a6604a72cd4'
+RoR::Application.config.secret_token = '8ee31ac87178dfee65b2e0d610b29bab1f62304685174dc1fb78be1338cc1d0d17c7174082e3ded0632e71faa00602dbb971904a592c8ec019d91465cf1a3f06'
